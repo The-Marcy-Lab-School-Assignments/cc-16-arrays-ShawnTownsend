da6c183e5633ddfb7e9143cfe9c702468beedfab
ShawnTownsend <stitans28@gmail.com>
2021-11-16 16:08:30 +0000

    Answer

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-//write your code here
+// Algorithm
+// check how many parameters are actually defined
+    // if(startIndex === undefined and deleteCount === undefined)
+        // we only have an array
+        // create a new array and fill it with all 'yerr'
+        // Array.prototype.fill()
+    
+    // if(deleteCount === undefined)
+        // replace the elements starting at startIndex until the end
+        // .slice() .splice()
+
+    // else
+        // replace the elements starting at startIndex
+        // but only replace deleteCount number of elements
+        // .slice() .splice()
+
+
+function replaceWithYerr(array, startIndex = 0, deleteCount = array.length){
+    let newArray = [...array]
+   
+    for(let i = startIndex; i < deleteCount + startIndex; i++) {
+        newArray[i] = 'yerr'
+    }
+    return newArray;
+}
+
+
+
+
